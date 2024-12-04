f39bbdb76d0439be76d75131e1e880829038f739
Gerard Harkema <ga.harkema@avans.nl>
2024-12-04 14:23:52 +0100

    20241204-1

--- 5_transferframes/transferframes/transferframes/assignment1.py
+++ 5_transferframes/transferframes/transferframes/assignment1.py
@@ -27,8 +27,11 @@ joint_names = [
 base_link_name = "link_base"
 end_effector_name = "link_eef"
 group_name = "xarm6"
-package_name = 'manipulation_moveit_config'
+package_name = 'transferframes_moveit_config'
 srdf_file_name = 'config/manipuation_environment.srdf'
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 
 joint_states = ['left', 'right', 'home']
 
@@ -43,6 +46,9 @@ def main():
     lite6_groupstates = srdfGroupStates(package_name, srdf_file_name, group_name)
     move_group_helper = MovegroupHelper(node, joint_names, base_link_name, end_effector_name, group_name)
 
+    camera = Camera()
+    result, photo = camera.take_photo()
+
     # Spin the node in background thread(s) and wait a bit for initialization
     executor = rclpy.executors.MultiThreadedExecutor(2)
     executor.add_node(node)
@@ -50,21 +56,100 @@ def main():
     executor_thread.start()
     node.create_rate(1.0).sleep()
 
+    # Move to joint configuration
+    result, joint_values = lite6_groupstates.get_joint_values('home')
+    if result:
+        print("Move to " + 'home')
+        move_group_helper.move_to_configuration(joint_values)
+    else:
+        print( "Failed to get joint_values of " + 'home')
 
-    for joint_state in joint_states:
+
+    result, photo = camera.take_photo()
+    if not result:
+        
+        return
+    
+    camera.destroy_node()
+    parts = photo['parts']
+    camera_frame = ['camera_frame']
+    print("Parts detected: ")
+    print(parts)
+    
+    for part in parts:
+        print(f'Handeling: {part}')
+
+
+        #print("Move to published fransfer frame")
+        to_frame_rel = 'xarm_link'
+        from_frame_rel = part
+        if 0:
+            try:
+                t = node.tf_buffer.lookup_transform(
+                    to_frame_rel,
+                    from_frame_rel,
+                    rclpy.time.Time())
+                #node.get_logger().info(t)
+                translation = [0.0, 0.0, 0.0]
+                rotation = [0.0, 0.0, 0.0, 0.0]
+
+                translation[0] = t.transform.translation.x
+                translation[1] = t.transform.translation.y
+                translation[2] = t.transform.translation.z
+                rotation[0] = t.transform.rotation.w
+                rotation[1] = t.transform.rotation.x
+                rotation[2] = t.transform.rotation.y
+                rotation[3] = t.transform.rotation.z
+                move_group_helper.move_to_pose(translation, rotation)
+            
+            except TransformException as ex:
+                node.get_logger().info(
+                    f'Could not transform {from_frame_rel} to {to_frame_rel}: {ex}')
+
+        ## goto pre-grasp
+        ## goto grasp
+        ## gripper enable
+        ## goto post-grasp
+
+
         # Move to joint configuration
-        result, joint_values = lite6_groupstates.get_joint_values(joint_state)
+        result, joint_values = lite6_groupstates.get_joint_values('home')
         if result:
-            print("Move to " + joint_state)
+            print("Move to " + 'home')
             move_group_helper.move_to_configuration(joint_values)
         else:
-            print( "Failed to get joint_values of " + joint_state)
+            print( "Failed to get joint_values of " + 'home')
 
 
-    print("Move to fixed pose")
-    translation = [0.5, 0.2, 0.25]
-    rotation = [1.0, 0.0, 0.0, 0.0]
-    move_group_helper.move_to_pose(translation, rotation)
+        # Move to joint configuration
+        result, joint_values = lite6_groupstates.get_joint_values('drop')
+        if result:
+            print("Move to " + 'drop')
+            move_group_helper.move_to_configuration(joint_values)
+        else:
+            print( "Failed to get joint_values of " + 'drop')
+
+        ## gripper release
+
+        # Move to joint configuration
+        result, joint_values = lite6_groupstates.get_joint_values('home')
+        if result:
+            print("Move to " + 'home')
+            move_group_helper.move_to_configuration(joint_values)
+        else:
+            print( "Failed to get joint_values of " + 'home')
+
+
+        pass
+
+
+    result, joint_values = lite6_groupstates.get_joint_values('resting')
+    if result:
+        print("Move to " + 'resting')
+        move_group_helper.move_to_configuration(joint_values)
+    else:
+        print( "Failed to get joint_values of " + 'resting')
+
 
 
     rclpy.shutdown()
