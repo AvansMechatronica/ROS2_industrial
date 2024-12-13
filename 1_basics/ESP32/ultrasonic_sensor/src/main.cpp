49bec9f45fdf4aae1f6d0c7752dd8820955b2be2
Gerard Harkema <ga.harkema@avans.nl>
2024-12-13 16:50:32 +0100

    20241213

--- 1_basics/ESP32/ultrasonic_sensor/src/main.cpp
+++ 1_basics/ESP32/ultrasonic_sensor/src/main.cpp
@@ -1,12 +1,7 @@
 #include <Arduino.h>
-
-#include <EEPROM.h>
-
 #include <micro_ros_platformio.h>
 
-#include <stdio.h>
 #include <rcl/rcl.h>
-#include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include "rosidl_runtime_c/string_functions.h"  // Header for string assignment functions
@@ -15,47 +10,33 @@
 #include <sensor_msgs/msg/range.h>
 #include <range_sensors_interfaces/msg/sensor_information.h>
 
-#include <micro_ros_platformio.h>
-
-#undef ARDUINO_ESP32S3_DEV
-
-#if defined(ARDUINO_ESP32S3_DEV)
-  #include <WS2812FX.h>
-  WS2812FX ws2812fxStatus = WS2812FX(1, RGB_BUILTIN, NEO_GRB + NEO_KHZ800);
-  #define RGB_BUILTIN 21
-  #define RGB_BRIGHTNESS 10 // Change white brightness (max 255)
-#endif
-
 #include <HCSR04.h>
 
-#define NODE_NAME "sensor_info_publisher"
-
-#define SR04_TRIG_PIN   2
-#define SR04_ECHO_PIN   3
+#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
+#error This example is only avaliable for Arduino framework with serial transport.
+#endif
 
 rcl_publisher_t sensor_information_publisher;
-
-rclc_executor_t executor;
-
 range_sensors_interfaces__msg__SensorInformation sensor_information;
 
+
+rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-
-#define STATUS_LED 3
-
 rcl_timer_t timer;
+
+#define NODE_NAME "sensor_info_publisher"
+
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-int scan_index = 0;
-
 bool errorLedState = false;
-#define STATUS_LED_PIN    8
-
+#define STATUS_LED_PIN    2 //LED_BUILTIN //8
 #define MAX_RANGE  1.00
 #define MIN_RANGE  0.10
+#define SR04_TRIG_PIN   32
+#define SR04_ECHO_PIN   33
 
 void error_loop(){
   Serial.printf("Ultrasonic Sensor\nError\nSystem halted");
@@ -86,70 +67,30 @@ void error_loop(){
   }
 }
 
-
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
   RCLC_UNUSED(last_call_time);
   if (timer != NULL) {
-
     struct timespec ts;
     extern int clock_gettime(clockid_t unused, struct timespec *tp);
     clock_gettime(CLOCK_REALTIME, &ts);
     sensor_information.sensor_data.header.stamp.sec = ts.tv_sec;
     sensor_information.sensor_data.header.stamp.nanosec = ts.tv_nsec;
 
-    double* distances = 0;// HCSR04.measureDistanceCm();
-    //Serial.printf("Distance = %f\n", (float)(distances[0]/100.0));
+    //double* distances = 0;// HCSR04.measureDistanceCm();
+    double* distances = HCSR04.measureDistanceCm();
     sensor_information.sensor_data.range= (float)(distances[0]/100.0);
 
     RCSOFTCHECK(rcl_publish(&sensor_information_publisher, &sensor_information, NULL));
   }
 }
 
-
 void setup() {
-
-  Serial.begin(115200);
-  while (!Serial);
-  delay(2000);
-  Serial.print("Sensor-controller started, node: ");
-  Serial.println(NODE_NAME);
-
-  //HCSR04.begin(SR04_TRIG_PIN, SR04_ECHO_PIN);
-
-#if defined(ARDUINO_ESP32S3_DEV)
-  ws2812fxStatus.init();
-  ws2812fxStatus.setMode(FX_MODE_STATIC);
-  ws2812fxStatus.setColor(RGB_BRIGHTNESS,0,0);
-#if 0
-  ws2812fxStatus.setBrightness(100);
-  ws2812fxStatus.setSpeed(200);
-  ws2812fxStatus.start();
-  ws2812fxStatus.service();
-#endif
-#else
-  pinMode(STATUS_LED_PIN, OUTPUT); 
-  digitalWrite(STATUS_LED_PIN, HIGH);
-#endif
-
-#if 1
-
-
-#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
-  #error This programm is only avaliable for Arduino framework with serial transport.
-#endif
-
+  // Configure serial transport
   Serial.begin(115200);
   set_microros_serial_transports(Serial);
   delay(2000);
 
-#if defined(ARDUINO_ESP32S3_DEV)
-  ws2812fxStatus.setColor(0, 0, RGB_BRIGHTNESS);
-  ws2812fxStatus.service();
-#endif
-
-
-  Serial.printf(" Rangesensor USB Connected\n");
-
+  HCSR04.begin(SR04_TRIG_PIN, SR04_ECHO_PIN);
   allocator = rcl_get_default_allocator();
 
   //create init_options
@@ -169,6 +110,7 @@ void setup() {
   sensor_information.sensor_data.min_range = (float)MIN_RANGE; // Minimum distance range of the sensor in m.
   sensor_information.sensor_data.max_range = MAX_RANGE; // Maximum distance range of the sensor in m.
 
+
   // create sensor_information_publisher
   RCCHECK(rclc_publisher_init_default(
     &sensor_information_publisher,
@@ -176,36 +118,23 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(range_sensors_interfaces, msg, SensorInformation),
     "sensor_info"));
 
-  // create timer,
 
+  // create timer,
+  const unsigned int timer_timeout = 1000;
   RCCHECK(rclc_timer_init_default(
     &timer,
     &support,
-    RCL_MS_TO_NS(100),
+    RCL_MS_TO_NS(timer_timeout),
     timer_callback));
 
   // create executor
-  int number_of_executors = 1;
-  RCCHECK(rclc_executor_init(&executor, &support.context, number_of_executors, &allocator));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
- 
-  Serial.println("Light-controller ready");
 
-#if defined(ARDUINO_ESP32S3_DEV)
-  ws2812fxStatus.setColor(0, RGB_BRIGHTNESS,0);
-#else
-  digitalWrite(STATUS_LED_PIN, LOW);
-#endif
-#endif
+
 }
 
 void loop() {
-  delay(10);
-#if 1
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
-
-#if defined(ARDUINO_ESP32S3_DEV)
-    ws2812fxStatus.service();
-#endif
-#endif
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
--- 3_navigation/navigation/launch/environmet.launch.py
+++ 3_navigation/navigation/launch/environmet.launch.py
@@ -1,80 +1,51 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
 import os
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.substitutions import FindPackageShare
-import subprocess
-from launch.event_handlers import OnProcessExit, OnProcessStart
+
+def setup_gzserver_launch(context, *args, **kwargs):
+    # Resolve the value of 'without_obstacles' at runtime
+    without_obstacles = LaunchConfiguration('without_obstacles').perform(context)
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    navigation_package_share_directory = get_package_share_directory('navigation')
+
+    if without_obstacles == 'True':  # Compare the string value
+        world_file = PathJoinSubstitution([navigation_package_share_directory, 'worlds', 'warehouse.sdf'])
+        return [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+                ),
+                launch_arguments={'world': world_file}.items()
+            )
+        ]
+    else:
+        world_file = PathJoinSubstitution([navigation_package_share_directory, 'worlds', 'warehouse_with_obstacles.sdf'])
+        return [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+                ),
+                launch_arguments={'world': world_file}.items()
+            )
+        ]
+
 
 def generate_launch_description():
 
-    # Declare launch arguments
-    world_name = LaunchConfiguration('world_name')
-    world_name_arg = DeclareLaunchArgument('world_name', default_value='warehouse_with_obstacles',
-                                       description='Choose the world to load: warehouse or warehouse_with_obstacles')
+    selector_arg = DeclareLaunchArgument('without_obstacles', default_value='False',
+                                       description='Choose True to load: warehouse_with_obstacles or False to load warehouse')
 
-    #launch_arguments={'gz_args': '-r {}'.format(world_name)}.items()
-    #print(launch_arguments)
-    #print(world_name.parse('world_name'))
-
-    navigation_package_share_directory = get_package_share_directory('navigation')
-    #world_file = PathJoinSubstitution([navigation_package_share_directory, 'worlds', str(world_name) + '.sdf'])
-    #print(world_file)
-
-    world_file = PathJoinSubstitution([navigation_package_share_directory, 'worlds', 'warehouse.sdf'])
-    world2 = PathJoinSubstitution([navigation_package_share_directory, 'worlds', 'warehouse_with_obstacles.sdf'])
-    
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-
-    # Paths to xacro and RViz config files
-    xacro_file = os.path.join(navigation_package_share_directory, 'urdf', 'environmet.urdf.xacro')
-
-    result = subprocess.run(['xacro', xacro_file], stdout=subprocess.PIPE, text=True)
-    
-    with open('tmp.urdf', "w") as urdf_file:
-        urdf_file.write(result.stdout)
-    
-
-    # Robot State Publisher Node
-    robot_state_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        parameters=[{
-            'robot_description': Command(['xacro ', xacro_file]),
-            'use_sim_time': True,
-       }]
-    )
-
-    gzserver_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-        ),
-        launch_arguments={'world': world_file}.items()
-    )
 
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
         )
-    )
-
-    # gazebo spawn entity node
-    environment_node = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        output='screen',
-        arguments=[
-            '-file', 'tmp.urdf',
-            '-entity', 'environmet',
-            '-timeout', str(50.0),
-        ],
-        parameters=[{'use_sim_time': True}],
     )
 
     x_pose = LaunchConfiguration('x_pose', default='0.0')
@@ -96,11 +67,11 @@ def generate_launch_description():
         ),
         launch_arguments={'use_sim_time': 'true'}.items()
     )
+
     return LaunchDescription([
-        world_name_arg,
-        gzserver_cmd,
+        selector_arg,
+        OpaqueFunction(function=setup_gzserver_launch),
     	gzclient_cmd,
-        #environment_node,
         spawn_turtlebot_cmd,
         robot_state_publisher_cmd,
     ])
