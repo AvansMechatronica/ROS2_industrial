70cde1742618b114bc7d667ba90e7a08e68090a2
Gerard Harkema <GerardAnneHarkema@gmail.com>
2025-03-28 12:31:44 +0100

    20250328

--- 4_manipulation/manipulation/launch/spawn_battery.launch.py
+++ 4_manipulation/manipulation/launch/spawn_battery.launch.py
@@ -3,6 +3,7 @@ from launch.actions import ExecuteProcess
 from ament_index_python import get_package_share_directory
 import random
 import math
+from launch_ros.actions import Node
 
 def generate_launch_description():
     pkg_path = get_package_share_directory('ros_industrial_gazebo')
@@ -10,15 +11,18 @@ def generate_launch_description():
 
     entity_name = 'battery_' + str(random.randint(0, 1000))
     
-    return LaunchDescription([
-        ExecuteProcess(
-            cmd=[
-                'ign', 'service', '-s', '/world/default/create',
-                '--reqtype', 'ignition.msgs.EntityFactory',
-                '--reptype', 'ignition.msgs.Boolean',
-                '--timeout', '2000',
-                '--req', f'name: "{entity_name}" sdf_filename: "{model_path}" pose: {{position: {{x: 0.4, y: -0.4, z: 1.5}}, orientation: {{w: {math.cos(math.radians(180) / 2)}, x: 0, y: 0, z: {math.sin(math.radians(180) / 2)}}}}}'
-            ],
-            output='screen'
-        )
-    ])
+    # ignition gazebo spawn entity node
+    gazebo_spawn_entity_node = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output='screen',
+        arguments=[
+            '-name', entity_name,
+            '-file', model_path,
+            '-x', '0.4', '-y', '-0.4', '-z', '1.5', '-P', str(math.radians(180)),
+        ],
+        #parameters=[{'use_sim_time': True}],
+    )
+
+
+    return LaunchDescription([gazebo_spawn_entity_node])
