24a2cfb307c57ff998754f03ed2d751afb9274bb
Gerard Harkema <ga.harkema@avans.nl>
2025-04-01 13:41:22 +0200

    20250401

--- 3_navigation/navigation/launch/navigate.launch.py
+++ 3_navigation/navigation/launch/navigate.launch.py
@@ -14,81 +14,130 @@
 #
 # Author: Darby Lim
 
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    GroupAction,
+    IncludeLaunchDescription,
+    OpaqueFunction
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import PushRosNamespace, SetRemap
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
 
 
-if 0:
-    import os
-
-    from ament_index_python.packages import get_package_share_directory
-    from launch import LaunchDescription
-    from launch.actions import DeclareLaunchArgument
-    from launch.actions import IncludeLaunchDescription
-    from launch.launch_description_sources import PythonLaunchDescriptionSource
-    from launch.substitutions import LaunchConfiguration
-    from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace, SetRemap
 
 
 
-    def generate_launch_description():
-        use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-        map_dir = LaunchConfiguration(
-            'map',
-            default=os.path.join(
-                get_package_share_directory('navigation'),
-                'maps',
-                'map_factory_v1.yaml'))
+ARGUMENTS = [
+    DeclareLaunchArgument('use_sim_time', default_value='false',
+                          choices=['true', 'false'],
+                          description='Use sim time'),
+    DeclareLaunchArgument('namespace', default_value='',
+                          description='Robot namespace'),
+    DeclareLaunchArgument('params_file',
+                          default_value=PathJoinSubstitution([
+                              get_package_share_directory('navigation'),
+                              'config',
+                              'nav2.yaml'
+                              ]),
+                          description='Nav2 parameters'),
+    DeclareLaunchArgument('namespace', default_value='',
+                          description='Robot namespace')
+]
 
-        map_dir = LaunchConfiguration(
-            'map',
-            default=os.path.join(
-                get_package_share_directory('navigation'),
-                'maps',
-                'map_warehouse_v1.yaml'))
+def launch_setup(context, *args, **kwargs):
+    pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
+
+    nav2_params = LaunchConfiguration('params_file')
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    namespace_str = namespace.perform(context)
+    if (namespace_str and not namespace_str.startswith('/')):
+        namespace_str = '/' + namespace_str
+
+    launch_nav2 = PathJoinSubstitution(
+        [pkg_nav2_bringup, 'launch', 'navigation_launch.py'])
+
+    nav2 = GroupAction([
+        PushRosNamespace(namespace),
+        SetRemap(namespace_str + '/global_costmap/scan', namespace_str + '/scan'),
+        SetRemap(namespace_str + '/local_costmap/scan', namespace_str + '/scan'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(launch_nav2),
+            launch_arguments=[
+                  ('use_sim_time', use_sim_time),
+                  ('params_file', nav2_params.perform(context)),
+                  ('use_composition', 'False'),
+                  ('namespace', namespace_str)
+                ]
+        ),
+    ])
+
+    return [nav2]
 
 
-        param_dir = LaunchConfiguration(
-            'params_file',
-            default=os.path.join(
-                get_package_share_directory('navigation'),
-                'turtlebot_parameters',
-                'turtlebot3.yaml'))
+def generate_launch_description():
+    pkg_navigation = get_package_share_directory('navigation')
+    pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
 
-        nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    localization_params_arg = DeclareLaunchArgument(
+        'params',
+        default_value=PathJoinSubstitution(
+            [pkg_navigation, 'config', 'localization.yaml']),
+        description='Localization parameters')
 
-        rviz_config_dir = os.path.join(
-            get_package_share_directory('navigation'),
-            'rviz',
-            'environment.rviz')
+    map_arg = DeclareLaunchArgument(
+        'map',
+        default_value=PathJoinSubstitution(
+            [pkg_navigation, 'maps', 'map_warehouse_v1.yaml']),
+        description='Full path to map yaml file to load')
 
-        return LaunchDescription([
-            DeclareLaunchArgument(
-                'map',
-                default_value=map_dir,
-                description='Full path to map file to load'),
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-            DeclareLaunchArgument(
-                'params_file',
-                default_value=param_dir,
-                description='Full path to param file to load'),
+    localization = GroupAction([
+        PushRosNamespace(namespace),
 
-            DeclareLaunchArgument(
-                'use_sim_time',
-                default_value='false',
-                description='Use simulation (Gazebo) clock if true'),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution(
+                    [pkg_nav2_bringup, 'launch', 'localization_launch.py'])),
+            launch_arguments={'namespace': namespace,
+                              'map': LaunchConfiguration('map'),
+                              'use_sim_time': use_sim_time,
+                              'params_file': LaunchConfiguration('params')}.items()),
+    ])
 
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
-                launch_arguments={
-                    'map': map_dir,
-                    'use_sim_time': use_sim_time,
-                    'params_file': param_dir}.items(),
-            ),
+    rviz_config_dir = os.path.join(get_package_share_directory('navigation'),
+                                    'rviz', 'environment.rviz')
 
-            Node(
+    rviz_node = Node(
                 package='rviz2',
                 executable='rviz2',
                 name='rviz2',
                 arguments=['-d', rviz_config_dir],
                 parameters=[{'use_sim_time': use_sim_time}],
-                output='screen'),
-        ])
+                output='screen')
+
+
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(localization_params_arg)
+    ld.add_action(map_arg)
+    ld.add_action(localization)
+    ld.add_action(rviz_node)
+    ld.add_action(OpaqueFunction(function=launch_setup))
+
+    return ld
+
+
