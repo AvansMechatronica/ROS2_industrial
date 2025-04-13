9ed1509274362037eeef10e139d8ecc3c57894fb
Gerard Harkema <ga.harkema@avans.nl>
2025-04-13 17:22:26 +0200

    20250413

--- support/ros_industrial_actuators/vacuum_gripper/VacuumGripper.cc
+++ support/ros_industrial_actuators/vacuum_gripper/VacuumGripper.cc
@@ -1,21 +1,59 @@
 #include "VacuumGripper.hh"
 
-#include <gz/msgs/int32.pb.h>
 
 #include <iostream>
 #include <vector>
 
-#include <gz/plugin/Register.hh>
-
 
 using namespace vacuum_gripper;
- 
-VacuumGripper::VacuumGripper()
+
+class vacuum_gripper::VacuumGripperPrivate
 {
+  public:
+    gz::transport::Node node_;
+    gz::transport::Node::Publisher status_pub_;
+
+    std::string namespace_ = "";
+    std::string enable_topic_ = "/enable";
+    std::string status_topic_ = "/status";
+  
+};
+
+VacuumGripper::VacuumGripper(): dataPtr(new VacuumGripperPrivate())
+{
+  CreatePublishers();
+  CreateSubscribers();
 }
  
 VacuumGripper::~VacuumGripper()
 {
+}
+
+void VacuumGripper::CreatePublishers()
+{
+  dataPtr->status_pub_ = gz::transport::Node::Publisher();
+  dataPtr->status_pub_ = dataPtr->node_.Advertise < gz::msgs::Boolean> (dataPtr->status_topic_);
+}
+
+void VacuumGripper::CreateSubscribers()
+{
+  dataPtr->node_.Subscribe(dataPtr->enable_topic_, &VacuumGripper::OnEnableMessage, this);
+}
+
+void VacuumGripper::RemovePublishers()
+{
+  dataPtr->node_.UnadvertiseSrv(dataPtr->status_topic_);
+}
+
+
+void VacuumGripper::RemoveSubscribers()
+{
+  dataPtr->node_.Unsubscribe(dataPtr->enable_topic_);
+}
+
+
+void VacuumGripper::OnEnableMessage(const gz::msgs::Boolean & msg){
+
 }
  
 void VacuumGripper::PostUpdate(const gz::sim::UpdateInfo &_info,
@@ -24,8 +62,18 @@ void VacuumGripper::PostUpdate(const gz::sim::UpdateInfo &_info,
   gzmsg << "VacuumGripper::PostUpdate" << std::endl;
 }
 
+#if 0
+void VacuumGripper::Update(const gz::sim::UpdateInfo &_info,
+  const gz::sim::EntityComponentManager &_ecm)
+{
+gzmsg << "VacuumGripper::Update" << std::endl;
+}
+#endif
+
 // Include a line in your source file for each interface implemented.
 GZ_ADD_PLUGIN(
   vacuum_gripper::VacuumGripper,
   gz::sim::System,
-  vacuum_gripper::VacuumGripper::ISystemPostUpdate)
+  vacuum_gripper::VacuumGripper::ISystemPostUpdate//,
+  //vacuum_gripper::VacuumGripper::ISystemUpdate
+)
--- support/ros_industrial_actuators/vacuum_gripper/VacuumGripper.hh
+++ support/ros_industrial_actuators/vacuum_gripper/VacuumGripper.hh
@@ -1,12 +1,23 @@
+#ifndef _VACUUM_GRIPPER_PLUGIN_INCL_
+#define _VACUUM_GRIPPER_PLUGIN_INCL_
 #include <gz/sim/System.hh>
+#include <gz/plugin/Register.hh>
+#include <gz/msgs.hh>
+#include <gz/transport/Node.hh>
+#include <gz/msgs/boolean.pb.h>
+
 
 namespace vacuum_gripper
 {
+
+  class VacuumGripperPrivate;
+
   class VacuumGripper:
     // This class is a system.
     public gz::sim::System,
     // This class also implements the ISystemPostUpdate interface.
-    public gz::sim::ISystemPostUpdate
+    public gz::sim::ISystemPostUpdate//,
+//    public gz::sim::ISystemUpdate
   {
     public: VacuumGripper();
  
@@ -14,6 +25,24 @@ namespace vacuum_gripper
  
     public: void PostUpdate(const gz::sim::UpdateInfo &_info,
                 const gz::sim::EntityComponentManager &_ecm) override;
+#if 0
+    public: void Update(const gz::sim::UpdateInfo &_info,
+                  const gz::sim::EntityComponentManager &_ecm) override;
+#endif
+    private: std::unique_ptr<VacuumGripperPrivate> dataPtr;
+
+    /// \brief Subscriber callbacks
+    private:
+    void OnEnableMessage(const gz::msgs::Boolean & msg);
+    void CreatePublishers();
+    void CreateSubscribers();
+    void RemovePublishers();
+    void RemoveSubscribers();
+
+
+
   };
- 
+
+
 }
+#endif // _VACUUM_GRIPPER_PLUGIN_INCL_
