c1533a26fdd6fa68f555cbaca85a658dbd223e9c
Gerard Harkema <ga.harkema@avans.nl>
2025-11-26 16:11:29 +0100

    20251126

--- install/install.bash
+++ install/install.bash
@@ -1,19 +1,13 @@
 #!/bin/bash
 sudo apt update
-sudo apt install ros-$ROS_DISTRO-gz-ros2-control -y
-sudo apt install ros-$ROS_DISTRO-joint-state-publisher-gui -y
-sudo apt install ros-$ROS_DISTRO-moveit* -y
-sudo apt install ros-$ROS_DISTRO-geometric-shapes
-sudo apt install ros-$ROS_DISTRO-srdfdom -y
-sudo apt update
-sudo apt install ros-$ROS_DISTRO-eigen-stl-containers
-sudo apt -y install ros-$ROS_DISTRO-tf-transformations
+# Install pip
 sudo apt install python3-pip -y
 
 # Environment Setup
 source /opt/ros/$ROS_DISTRO/setup.bash
 CURRENT_DIR=$(pwd)
 
+# Install specific versions of numpy and scipy
 pip install numpy==1.24.3 scipy==1.10.1
 
 # Update rosdep
@@ -22,10 +16,9 @@ rosdep update
 
 # Install dependencies for the current workspace
 cd $CURRENT_DIR/..
-rosdep install --from-paths install --ignore-src -r -y
+rosdep install --from-paths ros_industrial --ignore-src -r -y
 
-
-
+# Set QT_QPA_PLATFORM to xcb
 if ! env | grep -q "QT_QPA_PLATFORM=xcb"; then
     echo "export QT_QPA_PLATFORM=xcb" >> ~/.bashrc
 fi
@@ -39,7 +32,7 @@ fi
 XARM_DIR=~/xarm_ws
 if ! ros2 pkg list | grep -q "xarm_description"; then
     # Missing in xarm dependencys
-    sudo apt install ros-$ROS_DISTRO-moveit
+    #sudo apt install ros-$ROS_DISTRO-moveit
     echo "Cloning xarm packages"
     mkdir -p "$XARM_DIR/src"
     cd "$XARM_DIR/src"
